8effc35b2093da3a79b36c67dec23b99840c0115
Kane Koo <kanekoo@gmail.com>
2019-03-23 18:12:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+# Write your code here.
+
+def badge_maker(name)
+  str = "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(namelist)
+  
+  badgelist=[]
+  i=0 
+  length_name = namelist.length
+  for i in 0..(length_name-1)
+    name_mesg = badge_maker(namelist[i])
+    badgelist.push(name_mesg)
+  end
+  return (badgelist)
+  
+end
+
+
